e49997f164c449bc8511a7d8186af1970fc46797
heesuuuuu <heesu.dev@gmail.com>
2025-08-15 13:12:42 +0900

    update : 리뷰 기능 추가

--- products/admin.py
+++ products/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Review
 
-# admin 페이지에서 카테고리 직접 추가 가능 
-admin.site.register(Product)
-admin.site.register(Category)
+# # admin 페이지에서 카테고리 직접 추가 가능 
+# admin.site.register(Product)
+# admin.site.register(Category)
 
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'price', 'created_at')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+
+
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'rating', 'created_at')
+    list_filter = ('product', 'user', 'rating')
+#
--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, Category
-from .forms import ProductForm
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product, Category, Review
+from .forms import ProductForm, ReviewForm
 from django.db.models import Q
 
 def product_list(request):
@@ -26,7 +25,21 @@ def product_list(request):
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    return render(request, 'products/product_detail.html', {'product': product})
+    reviews = product.reviews.all() ## related_name을 사용하여 리뷰 가져오기
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.product = product
+            review.user = request.user
+            review.save()
+            return redirect('product_detail', pk=pk)
+    else:
+        form = ReviewForm()
+    return render(request, 'products/product_detail.html', {
+        'product': product,
+        'reviews': reviews,
+        'form': form})
 
 def product_create(request):
     if request.method == 'POST':
